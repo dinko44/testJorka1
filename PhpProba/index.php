33544096266b1f59c7788071cbb37fc9750bb337
root <nisait@abv.bg>
2016-10-17 18:18:37 +0300

    2-ra promiana

--- PhpProba/index.php
+++ PhpProba/index.php
@@ -13,6 +13,7 @@ and open the template in the editor.
         <?php
       echo ('Raboti li - test?');  // put your code here
       echo ('Nov red');
+      echo ('oshe edin red');
         ?>
     </body>
 </html>
